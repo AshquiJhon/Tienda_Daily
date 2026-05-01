ec34a7e7591dafa6b391b98649eb47350bfbe757
Jhon <ashquijhon26@gmail.com>
2026-05-01 08:47:43 -0500

    fix superuser 2

--- build.sh
+++ build.sh
@@ -4,15 +4,6 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-echo "Creando superusuario..."
+echo "CREANDO SUPERUSUARIO..."
 
-python manage.py shell -c "
-from django.contrib.auth import get_user_model;
-User = get_user_model();
-import os;
-username = 'admin';
-password = '123456';
-email = 'admin@gmail.com';
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
-"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); import os; username='admin'; password='123456'; email='admin@gmail.com'; User.objects.filter(username=username).exists() or User.objects.create_superuser(username, email, password)"
