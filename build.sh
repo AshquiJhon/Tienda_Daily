0af6c6e6c14b29bd229fb8829a99a509ab754383
Jhon <ashquijhon26@gmail.com>
2026-05-01 08:45:47 -0500

    fix superuser

--- build.sh
+++ build.sh
@@ -4,8 +4,15 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-python manage.py shell << END
-from django.contrib.auth.models import User
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@gmail.com", "123456")
-END
+echo "Creando superusuario..."
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+import os;
+username = 'admin';
+password = '123456';
+email = 'admin@gmail.com';
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+"
