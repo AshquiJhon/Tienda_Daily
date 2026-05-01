f5a116c779874c0b56380f107a0b701eb10de7c1
Jhon <ashquijhon26@gmail.com>
2026-05-01 08:43:18 -0500

    crear superusuario automatico

--- build.sh
+++ build.sh
@@ -3,3 +3,9 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+python manage.py shell << END
+from django.contrib.auth.models import User
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@gmail.com", "123456")
+END
